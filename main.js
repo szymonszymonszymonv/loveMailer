2370734b49fee85202f9e58c8e30c2068d0c0073
Szymon Prokopiuk <kalarepka8799@gmail.com>
2018-09-26 13:33:30 +0200

    make code more readable and also functional

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 'use strict'
-require('module')
-console.log(__filename)
-// var nodemailer = require('C:/Users/Szymon/Documents/PROJECTS/loveMailer/node_modules/nodemailer')
+var nodemailer = require('nodemailer')
+console.log(nodemailer)
 var love = {
+  config: {
+    service: 'gmail', // your service
+    auth: {
+      user: 'secret', // your email
+      pass: 'secret' // your password
+    }
+  },
+
+  mailOptions: {
+    from: 'secret', // you
+    to: 'secret', // your loved one
+    subject: 'love',
+    text: this.generateMessage()
+  },
+
+  transporterConstruct: function () { // create transporter object based on configs
+    var config = this.config
+    return nodemailer.createTransport(config)
+  },
+
+  transporter: this.transporterConstruct(),
   anniversary: {
+
     date: new Date(/* year, month(0-11 not 1-12), day */), // start
     currentDate: new Date(),
+
     length: function () { // returns length in months, use it as u wish
       var date = this.date
       var difference
@@ -13,6 +35,7 @@ var love = {
       difference += this.currentDate.getMonth() - date.getMonth()
       return difference
     },
+
     isit: function () { // true if it is your anniversary and false if it isnt - it will determine whether or not the message is sent
       if (this.date.getDate() === this.currentDate.getDate()) {
         return true
@@ -20,44 +43,36 @@ var love = {
         return false
       }
     }
+
   },
+
   messages: [],
+
   createMessage: function (string) { // create multiple messages so that you can use one of them randomly - EXAMPLE: 'hi sweet i love you' // use this.anniversary.length
     this.messages.push(string)
   },
 
-  generateMessage: function () {
+  generateMessage: function () { // randomly generates a message from your array
     var index = Math.floor(Math.random() * this.messages.length)
     return this.messages[index]
+  },
+
+  sendMessage: function () {
+    this.transporter.sendMail(this.mailOptions, function (error, info) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('Email sent: ' + info.response)
+      }
+    })
   }
-}
-love.createMessage('hej kici')
-console.log(love.generateMessage())
-/*
-var config = {
-  service: 'gmail',
-  auth: {
-    user: 'name@email.com',
-    pass: 'encoded password'
-  }
-}
-var transporter = nodemailer.createTransport(config)
-console.log(transporter)
-var mailOptions = {
-  from: config.auth.user, // you
-  to: 'target@email.com', // your loved one
-  subject: 'subject',
-  text: 'your message'
 }
 
-transporter.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log('Email sent: ' + info.response)
-  }
-})
-*/
-//  TODO: figure out what to do with anniversaries
+// love.createMessage('hej kici')
+// love.createMessage('przytulkam cie')
+// love.createMessage('ale jestes fajna')
+// love.sendMessage()
+
+//  TODO: i want it to be a cool app with an interface so maybe lets use react or angular
 //  TODO: learn how to program
 //  TODO: get a brain
