10e6ce172270517163de92a441bdec7706950cd4
Szymon Prokopiuk <kalarepka8799@gmail.com>
2018-10-04 11:53:08 +0200

    create createMessages

--- src/CreateMessage.js
+++ src/CreateMessage.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
 class CreateMessage extends Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.props.onMessageChange(e.target.value)
+  }
+
   render () {
+    const message = this.props.message
     return (
       <div className='box'>
         <p>create a message!</p>
-        <textarea rows='10' cols='50'></textarea>
-        <button type='button'>send</button>
+        <textarea rows='10' cols='50' value={ message } onChange={ this.handleChange }></textarea>
+        <br></br>
       </div>
     )
   }
